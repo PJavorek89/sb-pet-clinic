be655c86c3d891dd6c98beb23080601b627c2aea
PJavorek89 <pavel.javorek89@gmail.com>
2022-01-19 22:01:06 +0100

    add changes from orig.

---
+++ pet-clinic-web/src/main/java/com/petClinic/javorek/petclinic/bootstrap/dataLoader.java
@@ -0,0 +1,18 @@
+package com.petClinic.javorek.petclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class dataLoader implements CommandLineRunner {
+
+
+
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
